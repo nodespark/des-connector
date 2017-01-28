78c25dd4db2b9e0cdc952e39ead3f37634be053d
Lachezar Valchev <lachezar@yachtico.com>
2017-01-28 18:23:55 +0200

    NOISSUE by lachezar.valchev: Implemented Children, DateRange, Histogram, Missing and Range bucket aggregations.

--- src/DESConnector/Elasticsearch/Aggregations/Bucket/Children.php
+++ src/DESConnector/Elasticsearch/Aggregations/Bucket/Children.php
@@ -10,4 +10,18 @@ namespace nodespark\DESConnector\Elasticsearch\Aggregations\Bucket;
 class Children extends Bucket
 {
 
+    const TYPE = 'children';
+
+    /**
+     * @param string $aggrName
+     * @param string $aggrFieldName
+     */
+    public function __construct($aggrName, $aggrFieldName)
+    {
+        parent::__construct($aggrName, $aggrFieldName);
+
+        $this->removeParameter('field');
+        $this->addParameter('type', $this->getFieldName());
+    }
+
 }
