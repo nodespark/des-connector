973293db881f6d2100a1fefadc4ec8bdd8ee0d39
Lachezar Valchev <lachezar@yachtico.com>
2017-01-28 18:26:30 +0200

    NOISSUE by lachezar.valchev: Made some initial steps in the implementation of the Filter bucket aggregation which should be handled as a corner case.

--- src/DESConnector/Elasticsearch/Aggregations/Bucket/Filter.php
+++ src/DESConnector/Elasticsearch/Aggregations/Bucket/Filter.php
@@ -2,12 +2,20 @@
 
 namespace nodespark\DESConnector\Elasticsearch\Aggregations\Bucket;
 
+use nodespark\DESConnector\Elasticsearch\Aggregations\AggregationInterface;
+
 /**
  * Class Filter
  *
  * @package nodespark\DESConnector\Elasticsearch\Aggregations\Bucket
  */
-class Filter extends Bucket
+class Filter implements AggregationInterface
 {
+    const TYPE = 'filter';
 
+    // TODO: To be implemented as special case!
+
+  public function getName() {
+    // TODO: Implement getName() method.
+  }
 }
